bbd47e3d29c2fcbbaf03bc6fb3fcfed1bd3ef423
pratapayush4849 <pratapayush4849@gmail.com>
2025-05-09 09:19:08 +0000

    9may

--- lib/frontend/login_screen.dart
+++ lib/frontend/login_screen.dart
@@ -69,35 +69,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       children: [
                         Admin(),
                         SizedBox(width: global.SizeConfig.screenWidth * 0.05),
-
-                        CupertinoButton(
-                          onPressed: () => _showAlertDialog,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              CircleAvatar(
-                                backgroundColor: CupertinoColors.white,
-                                maxRadius:
-                                    global.SizeConfig.screenHeight * 0.05,
-                                minRadius:
-                                    global.SizeConfig.screenHeight * 0.04,
-                                foregroundImage: AssetImage(
-                                  'assets/images/avatar/Test.png',
-                                ),
-                              ),
-                              Text(
-                                'Test',
-                                style: TextStyle(
-                                  fontFamily: 'Blanka',
-                                  color: CupertinoColors.black,
-                                  fontSize:
-                                      global.SizeConfig.screenHeight * 0.03,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                        Test(),
                       ],
                     ),
                   ),
@@ -106,78 +78,9 @@ class _LoginScreenState extends State<LoginScreen> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        CupertinoButton(
-                          onPressed:
-                              () => Navigator.of(context).push(
-                                CupertinoPageRoute(
-                                  builder: (context) => HomeScreen(),
-                                ),
-                              ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              CircleAvatar(
-                                backgroundColor: CupertinoColors.white,
-                                maxRadius:
-                                    global.SizeConfig.screenHeight * 0.05,
-                                minRadius:
-                                    global.SizeConfig.screenHeight * 0.04,
-                                foregroundImage: AssetImage(
-                                  'assets/images/avatar/Pika.png',
-                                ),
-                              ),
-                              Text(
-                                'Pika',
-                                style: TextStyle(
-                                  fontFamily: 'Blanka',
-                                  color: CupertinoColors.black,
-                                  fontSize:
-                                      global.SizeConfig.screenHeight * 0.03,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                        Pika(),
                         SizedBox(width: global.SizeConfig.screenWidth * 0.1),
-
-                        CupertinoButton(
-                          onPressed: () {
-                            // Suggested code may be subject to a license. Learn more: ~LicenseLog:2531664692.
-                            print(
-                              Geolocator.getCurrentPosition(
-                                locationSettings: LocationSettings(
-                                  accuracy: LocationAccuracy.high,
-                                ),
-                              ),
-                            );
-                          },
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              CircleAvatar(
-                                backgroundColor: CupertinoColors.white,
-                                maxRadius:
-                                    global.SizeConfig.screenHeight * 0.05,
-                                minRadius:
-                                    global.SizeConfig.screenHeight * 0.04,
-                                foregroundImage: AssetImage(
-                                  'assets/images/avatar/Panda.png',
-                                ),
-                              ),
-                              Text(
-                                'Panda',
-                                style: TextStyle(
-                                  fontFamily: 'Blanka',
-                                  color: CupertinoColors.black,
-                                  fontSize:
-                                      global.SizeConfig.screenHeight * 0.03,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                        Panda(),
                       ],
                     ),
                   ),
@@ -226,57 +129,193 @@ class _AdminState extends State<Admin> {
         ],
       ),
       onPressed: () {
-        CupertinoAlertDialog(
-          title: Text('Alert Title'),
-          content: Text('This is the content of the alert.'),
-          actions: [
-            CupertinoDialogAction(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            CupertinoDialogAction(
-              child: Text('OK'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+        showCupertinoDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return CupertinoAlertDialog(
+              title: Text('Alert Dialog'),
+              content: Text('This is a Cupertino Alert Dialog.'),
+              actions: [
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
         );
       },
     );
   }
 }
 
-void _showAlertDialog(BuildContext context) {
-  showCupertinoDialog<void>(
-    context: context,
-    builder:
-        (BuildContext context) => CupertinoAlertDialog(
-          title: const Text('Alert'),
-          content: const Text('Proceed with destructive action?'),
-          actions: <CupertinoDialogAction>[
-            CupertinoDialogAction(
-              /// This parameter indicates this action is the default,
-              /// and turns the action's text to bold text.
-              isDefaultAction: true,
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('No'),
+class Pika extends StatefulWidget {
+  Pika({Key? key}) : super(key: key);
+
+  @override
+  _PikaState createState() => _PikaState();
+}
+
+class _PikaState extends State<Pika> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      pressedOpacity: 0.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: CupertinoColors.white,
+            maxRadius: global.SizeConfig.screenHeight * 0.05,
+            minRadius: global.SizeConfig.screenHeight * 0.04,
+            foregroundImage: AssetImage('assets/images/avatar/Pika.png'),
+          ),
+          Text(
+            'ADMIN',
+            style: TextStyle(
+              fontFamily: 'Blanka',
+              color: CupertinoColors.black,
+              fontSize: global.SizeConfig.screenHeight * 0.03,
             ),
-            CupertinoDialogAction(
-              /// This parameter indicates the action would perform
-              /// a destructive action such as deletion, and turns
-              /// the action's text color to red.
-              isDestructiveAction: true,
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Yes'),
+          ),
+        ],
+      ),
+      onPressed: () {
+        showCupertinoDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return CupertinoAlertDialog(
+              title: Text('Alert Dialog'),
+              content: Text('This is a Cupertino Alert Dialog.'),
+              actions: [
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class Panda extends StatefulWidget {
+  Panda({Key? key}) : super(key: key);
+
+  @override
+  _PandaState createState() => _PandaState();
+}
+
+class _PandaState extends State<Panda> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      pressedOpacity: 0.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: CupertinoColors.white,
+            maxRadius: global.SizeConfig.screenHeight * 0.05,
+            minRadius: global.SizeConfig.screenHeight * 0.04,
+            foregroundImage: AssetImage('assets/images/avatar/Panda.png'),
+          ),
+          Text(
+            'ADMIN',
+            style: TextStyle(
+              fontFamily: 'Blanka',
+              color: CupertinoColors.black,
+              fontSize: global.SizeConfig.screenHeight * 0.03,
             ),
-          ],
-        ),
-  );
+          ),
+        ],
+      ),
+      onPressed: () {
+        showCupertinoDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return CupertinoAlertDialog(
+              title: Text('Alert Dialog'),
+              content: Text('This is a Cupertino Alert Dialog.'),
+              actions: [
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      pressedOpacity: 0.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: CupertinoColors.white,
+            maxRadius: global.SizeConfig.screenHeight * 0.05,
+            minRadius: global.SizeConfig.screenHeight * 0.04,
+            foregroundImage: AssetImage('assets/images/avatar/Test.png'),
+          ),
+          Text(
+            'ADMIN',
+            style: TextStyle(
+              fontFamily: 'Blanka',
+              color: CupertinoColors.black,
+              fontSize: global.SizeConfig.screenHeight * 0.03,
+            ),
+          ),
+        ],
+      ),
+      onPressed: () {
+        showCupertinoDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return CupertinoAlertDialog(
+              title: Text('Alert Dialog'),
+              content: Text('This is a Cupertino Alert Dialog.'),
+              actions: [
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
 }
